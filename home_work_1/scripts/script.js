d1f96fa25bbe983642d3c8e3e569d8315407ac1f
oleGAPrelev <101264107+oleGAPrelev@users.noreply.github.com>
2022-06-17 18:34:04 +0200

    timeout added

--- home_work_1/scripts/script.js
+++ home_work_1/scripts/script.js
@@ -1,5 +1,15 @@
-// Примените стили к карточкам: width, padding, border, background - color.
+// 1. Задан массив объектов workers.
 
+// 2. Выведите на экран карточки со следующей информацией по всем
+// работникам: ID, Имя, Фамилия, Возраст, Зарплата, фото.
+
+// 3. Зарплата рассчитывается через умножение ставки на количество дней.
+
+// 4. Карточки должны включать отображение фотографии.
+
+// 5. Примените стили к карточкам: width, padding, border, background - color.
+
+// 1
 const workers = [
 	{
 		id: 1,
@@ -10,7 +20,7 @@ const workers = [
 		days: 15,
 		photo: 'https://reqres.in/img/faces/12-image.jpg',
 		progress: 50,
-		email: 'olga.petrova@MediaList.ru',
+		// email: 'aprelev.o@MediaList.ru',
 	},
 
 	{
@@ -22,7 +32,7 @@ const workers = [
 		days: 22,
 		photo: 'https://reqres.in/img/faces/4-image.jpg',
 		progress: 10,
-		email: 'petr.ivanov@MediaList.ru',
+		// email: 'aprelev.o@MediaList.ru',
 	},
 
 	{
@@ -34,7 +44,7 @@ const workers = [
 		days: 10,
 		photo: 'https://reqres.in/img/faces/10-image.jpg',
 		progress: 75,
-		email: 'oleg.orlov@MediaList.ru',
+		// email: 'aprelev.o@MediaList.ru',
 	},
 
 	{
@@ -46,61 +56,71 @@ const workers = [
 		days: 17,
 		photo: 'https://reqres.in/img/faces/2-image.jpg',
 		progress: 30,
-		email: 'irina.medvedeva@MediaList.ru',
+		// email: 'aprelev.o@MediaList.ru',
 	},
 ];
 
+// 2
 const rootElem = document.querySelector('#root');
 workers.forEach(function ({
 	id,
 	first_name,
 	last_name,
 	age,
-	salary,
 	rate,
 	days,
 	photo,
 	progress,
+	// email,
 }) {
 	const container = document.createElement('div');
 	const idElem = document.createElement('p');
-	const firstNameElem = document.createElement('p');
-	const lastNameElem = document.createElement('p');
+	const fristNameElem = document.createElement('p');
+	const lastNameElemr = document.createElement('p');
 	const ageElem = document.createElement('p');
 	const salaryElem = document.createElement('p');
+	// const emailElem = document.createElement('a');
+	const textElem = document.createElement('p');
 	const photoElem = document.createElement('img');
-	photoElem.setAttribute('src', photo);
-	photoElem.setAttribute('alt', 'photo of worker');
-	const progrElem = document.createElement('progress');
-	progrElem.setAttribute('value', progress);
-	progrElem.setAttribute('max', '100');
-	container.style.cssText = `
-    background-color: #FFE4C4;
-    border: 2px solid black;
-    width: 200px;
-    padding: 20px;
-    margin-bottom: 15px;
-    `;
-	progrElem.style.cssText = `
-    width: 80%;
-    height: 20px;
-    border: 2px solid black;
-    margin-top: 10px;
-    background-color: #FFE4C4;
-    `;
-	idElem.innerText = `iD: ${id}`;
-	firstNameElem.innerText = `First name: ${first_name}`;
-	lastNameElem.innerText = `Last name: ${last_name}`;
+	const progressContainer = document.createElement('div');
+	const progressLine = document.createElement('div');
+	const progressValue = document.createElement('p');
+
+	idElem.innerText = `id: ${id}`;
+	fristNameElem.innerText = `First name: ${first_name}`;
+	lastNameElemr.innerText = `Last name: ${last_name}`;
 	ageElem.innerText = `Age: ${age}`;
 	salaryElem.innerText = `Salary: ${rate * days}`;
+	progressValue.innerText = progress + '%';
+	// emailElem.innerText = email;
+	// textElem.innerText = `Email: ${email}`;
+
+	photoElem.setAttribute('src', photo);
+	photoElem.setAttribute('alt', 'photo of worker');
+	// emailElem.setAttribute('href', `mailto: ${email}`);
+
+	container.classList.add('container');
+	progressContainer.classList.add('progress-container');
+	progressLine.classList.add('progress-line');
+	progressValue.classList.add('progress-value');
+
+	setTimeout(() => {
+		progressLine.style.opacity = 1;
+		progressLine.style.width = progress + '%';
+	}, 1500);
+
+	progressContainer.append(progressLine, progressValue);
+
 	container.append(
 		idElem,
-		firstNameElem,
-		lastNameElem,
+		fristNameElem,
+		lastNameElemr,
 		ageElem,
 		salaryElem,
+		// emailElem,
+		textElem,
 		photoElem,
-		progrElem
+		progressContainer
 	);
 	rootElem.append(container);
 });
